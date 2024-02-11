71ea5d9e0a567ad6a97323749ebc29524c84d6bd
tanyginvv <tanyginvv@mail.ru>
2024-02-11 16:20:13 +0300

    сделал 1 задание 1 лабы и второе draganddrop

---
+++ lab1_1/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab1_1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+        private void Grid_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            var grid = sender as Grid;
+            if (grid != null)
+            {
+                foreach (var child in grid.Children)
+                {
+                    if (child is Grid innerGrid)
+                    {
+                        StartAnimation(innerGrid);
+                    }
+                }
+            }
+        }
+
+        private async void StartAnimation(Grid grid)
+        {
+            double acceleration = 0.1;
+            double moveDistance = 100;
+            double maxSpeed = Math.Sqrt(2 * moveDistance * acceleration);
+            double initialTopMargin = grid.Margin.Top; 
+            bool moving = true;
+
+            while (moving)
+            {
+                double targetY = initialTopMargin - moveDistance;
+                double initialSpeedUp = maxSpeed;
+                double speedUp = initialSpeedUp;
+
+                while (grid.Margin.Top > targetY && speedUp > 0)
+                {
+                    speedUp -= acceleration;
+                    grid.Margin = new Thickness(grid.Margin.Left, grid.Margin.Top - speedUp, grid.Margin.Right, grid.Margin.Bottom);
+                    await Task.Delay(16); 
+                }
+
+                double initialSpeedDown = 0; 
+                double speedDown = initialSpeedDown;
+
+                while (grid.Margin.Top < initialTopMargin && speedDown < maxSpeed)
+                {
+                    speedDown += acceleration;
+                    grid.Margin = new Thickness(grid.Margin.Left, grid.Margin.Top + speedDown, grid.Margin.Right, grid.Margin.Bottom);
+                    await Task.Delay(16);
+                }
+ 
+                moving = false;
+            }
+        }
+    }
+}
---
+++ lab1_2/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab1_2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private bool isDragging;
+        private Point startPoint;
+
+        public void YourWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                isDragging = true;
+                startPoint = e.GetPosition(canvas);
+                canvas.CaptureMouse();
+            }
+        }
+
+        private void Grid_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDragging)
+            {
+                Point currentPoint = e.GetPosition(canvas);
+                double deltaX = currentPoint.X - startPoint.X;
+                double deltaY = currentPoint.Y - startPoint.Y;
+
+                foreach (UIElement child in groupCanvas.Children)
+                {
+                    Canvas.SetLeft(child, Canvas.GetLeft(child) + deltaX);
+                    Canvas.SetTop(child, Canvas.GetTop(child) + deltaY);
+                }
+
+                startPoint = currentPoint;
+            }
+        }
+
+        private void Grid_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                isDragging = false;
+                canvas.ReleaseMouseCapture();
+            }
+        }
+    }
+}
